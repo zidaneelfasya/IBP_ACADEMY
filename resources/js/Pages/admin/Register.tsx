a64819a48fc63e9fc7c5f444f4ce991f44356f71
Zidane Elfasya <111755264+zidaneelfasya@users.noreply.github.com>
2025-08-01 14:55:12 +0700

    update function api

--- resources/js/Pages/admin/Register.tsx
+++ resources/js/Pages/admin/Register.tsx
@@ -23,7 +23,7 @@ export default function Register() {
     const submit: FormEventHandler = (e) => {
         e.preventDefault();
 
-        post(route('register'));
+        post(route('register.admin'));
     };
 
     return (
